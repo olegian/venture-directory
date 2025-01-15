b4f2c1cdc04ae9ef2096bb59601b545638e33b9d
Oleg Ianchenko <oleg.ianch@gmail.com>
2025-01-14 18:42:07 -0800

    progress!!!

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ extern crate crossterm;
 mod constants;
 mod types;
 use crossterm::{
-    cursor::{MoveDown, MoveToRow},
-    style::Print,
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    cursor::{MoveDown, MoveLeft, MoveRight, MoveTo, MoveToRow}, event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers}, style::{Print, Stylize}, terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType}
 };
 use std::{
     collections::VecDeque,
@@ -83,32 +81,145 @@ fn display_path_at_row(stdout: &mut Stdout, dirs: &Vec<Directory>, row_idx: u16,
     queue!(stdout, MoveToRow(row_idx), Print(full_name)).unwrap();
 }
 
-fn display_dirs(stdout: &mut Stdout, dirs: &Vec<Directory>, row_idx: u16, config: &Config) {
-    let n_dirs = dirs.len();
-    let show_idx = std::cmp::max(0, n_dirs - config.max_directory_depth);
-    let shown_dirs = &dirs[(config.max_directory_depth - show_idx)..];
-    let entry_iters = shown_dirs.iter().map(|dir| dir.contents.iter());
+fn get_entry_display_start_idx(dir: &Directory, config: &Config) -> usize {
+    if dir.selected_idx < config.n_rows {
+        // if the selected index is inbounds of the number of rows we are showing, then show from the start
+        return 0;
+    }
 
-    for _ in 0..config.n_rows {
-        entry_iters.
+    if dir.selected_idx + config.n_rows > dir.contents.len() {
+        // if the selected index is towards the end, show the last page
+        return dir.contents.len() - config.n_rows;
+    }
+
+    // selected index is out of bounds of n rows, and not towards the end, so start from wherever in the list
+    return dir.selected_idx;
+}
+
+fn display_dirs(stdout: &mut Stdout, dirs: &Vec<Directory>, row_idx: u16, config: &Config) {
+    queue!(stdout, MoveTo(10, 10));
+
+    let n_dirs = dirs.len();
+    let n_show = if n_dirs < config.max_directory_depth {
+        n_dirs
+    } else {
+        config.max_directory_depth
+    };
+
+    let shown_dirs = &dirs[n_dirs - n_show..];
+
+    let mut dirs_with_idxs = shown_dirs
+        .iter()
+        .map(|d| (d, get_entry_display_start_idx(d, config)))
+        .cycle();
+
+    for e_offset in 0..config.n_rows {
+        let mut line_len: u16 = 2;
+        queue!(stdout, Print("| "));
+        for dir_idx in 0..dirs.len() {
+            let current_dir = dirs_with_idxs.next();
+            match current_dir {
+                Some((d, start)) => {
+                    let mut repr = d.get_padded_entry_repr(
+                        start + e_offset,
+                        dir_idx == dirs.len() - 1,
+                        config,
+                    );
+
+                    line_len += repr.content().len() as u16;
+
+                    queue!(stdout, Print(repr));
+                }
+                None => panic!("Cycled iterator returned none. Was there nothing in shown_dirs?"),
+            }
+
+            queue!(stdout, Print(" |"));
+            line_len += 2
+        }
+
+        queue!(stdout, MoveLeft(line_len), MoveDown(1)).unwrap();
     }
 }
 
 fn main() {
     let mut config: Config = Default::default();
+    config.max_col_width = 20;
+    config.max_directory_depth = 5;
+    config.n_rows = 20;
+
     let mut stdout = stdout();
     let invoked_dir = Path::new("./").canonicalize().unwrap();
-
     let mut dirs: Vec<Directory> = init_directories(invoked_dir);
 
-    queue!(stdout, Clear(ClearType::All)).unwrap();
+    enable_raw_mode().unwrap();
+    loop {
+        queue!(stdout, Clear(ClearType::All)).unwrap();
+        display_path_at_row(&mut stdout, &dirs, 2, &config);
+        display_dirs(&mut stdout, &dirs, 5, &config);
+        stdout.flush().unwrap();
 
-    display_path_at_row(&mut stdout, &dirs, 2, &config);
-    display_dirs(&mut stdout, &dirs, 5, &config);
+        match read().unwrap() {
+            Event::Key(KeyEvent {
+                code: KeyCode::Left,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state,
+            }) => {
+                if dirs.len() <= 1 {
+                    continue;
+                }
 
-    queue!(stdout, MoveDown(20)).unwrap();
+                dirs.pop();
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Right,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state,
+            }) => {
+                execute!(stdout, Print("LEFT")).unwrap();
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Up,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state,
+            }) => {
+                let end = dirs.len() - 1;
+                dirs.get_mut( end).unwrap().select_prev();
+
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Down,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state,
+            }) => {
+                let end = dirs.len() - 1;
+                dirs.get_mut( end).unwrap().select_next();
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Esc,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state,
+            }) => break,
+            _ => (),
+        }
+    }
+
+    /*
+     TODO:
+     - Consider the ordering of the dirs vector, because currently we have to use get_mut to the last element which is im pretty sure worse than accesssing the front, and we only need one ended access honestly
+     - Shit scrolling feel especially when approaching out of n_rows bound
+     - Somehow get rid of screen flashes on action (could be cause by double buffering, clear only changed contents somehow?)
+     - Finish opening directories
+     - Return final path
+     - Styling
+    */
+
+    disable_raw_mode().unwrap();
+
+    queue!(stdout, MoveDown(30)).unwrap();
     stdout.flush().unwrap();
-
-    // dirs.iter().for_each(|e| {println!("{}", e.name)});
-    // // println!("{dirs:?}")
 }
